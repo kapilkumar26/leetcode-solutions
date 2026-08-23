88b9fec45925d26b0be6df7fb7ad79966fa3d4d8
kapil kumar <ksharma1048@gmail.com>
2026-08-23 22:15:49 +0530

    Time: 2 ms (11.43%), Space: 42.6 MB (96.14%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String interpret(String command) {
+       String ans="";
+       for(int i=0; i<command.length(); i++){
+         if(command.charAt(i)=='G'){
+            ans+="G";
+         }
+         else if(command.charAt(i)=='('){
+            if(command.charAt(i+1)==')'){
+                ans += "o";
+                i++;
+            }
+            else{
+                ans+="al";
+                i=i+3;
+            }
+         }
+       }
+       return ans; 
+    }
+}
